a5b5e0e4a96c7aa7ea88b7b7028f90262ec74400
SathvikReddy1363 <133073341+Sathvik1363@users.noreply.github.com>
2023-11-12 16:04:51 +0530

    Add files via upload

---
+++ task5/t5.js
@@ -0,0 +1,33 @@
+// Window alert message
+window.alert("Welcome to project1 of ksp");
+// printing string in js
+console.log("sathvik reddy");
+
+// variables
+var1 = 100;
+var2 = 4;
+var3 = 55;
+console.log(var1);
+console.log(var2);
+
+// camelCase
+ageOfAPerson = 27;
+console.log(ageOfAPerson);
+
+heightOfAPerson = 6.1;
+console.log(heightOfAPerson);
+
+// variables with proper declaration
+let name = "Sathvik Reddy";
+let age = 28;
+let height = 6.1;
+console.log(name);
+
+var name1 = "sathvik"
+var age1 = 29;
+var height1 = 6.2;
+console.log(height1)
+
+const PI = 3.14;
+console.log(PI);
+
